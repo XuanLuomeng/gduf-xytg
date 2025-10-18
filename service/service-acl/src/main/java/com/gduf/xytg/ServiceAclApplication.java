5131bb34b245f300eb90894fff348a7303ea6c1d
luojiawei <1079716693@qq.com>
2025-10-18 22:27:09 +0800

    010-复代码文件夹目录位置错误

--- service/service-acl/src/main/java/com/gduf/xytg/ServiceAclApplication.java
+++ service/service-acl/src/main/java/com/gduf/xytg/ServiceAclApplication.java
@@ -1,10 +1,19 @@
 package com.gduf.xytg;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @author LuoXuanwei
  * @version 1.0
- * @description 启动类
+ * @description 权限管理模块启动类
  * @date 2025/10/18 22:22
  */
+@SpringBootApplication
 public class ServiceAclApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceAclApplication.class, args);
+    }
+
 }
