4e546d29ce7f88b53262f44b4b710075271ce637
luojiawei <1079716693@qq.com>
2025-10-18 21:46:53 +0800

    006-编写项目公共实体类

---
+++ model/src/main/java/xytg/vo/product/SkuInfoVo.java
@@ -0,0 +1,22 @@
+package xytg.vo.product;
+
+import xytg.model.product.*;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class SkuInfoVo extends SkuInfo {
+
+	@ApiModelProperty(value = "海报列表")
+	private List<SkuPoster> skuPosterList;
+
+	@ApiModelProperty(value = "属性值")
+	private List<SkuAttrValue> skuAttrValueList;
+
+	@ApiModelProperty(value = "图片")
+	private List<SkuImage> skuImagesList;
+
+}
+
