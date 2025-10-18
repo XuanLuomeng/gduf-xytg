f6209a9f856f468a463d89851506548555f730a4
luojiawei <1079716693@qq.com>
2025-10-18 22:23:58 +0800

    007-修复代码文件夹目录位置错误

---
+++ model/src/main/java/com/xytg/vo/product/SkuInfoVo.java
@@ -0,0 +1,26 @@
+package com.xytg.vo.product;
+
+import com.xytg.model.product.SkuAttrValue;
+import com.xytg.model.product.SkuImage;
+import com.xytg.model.product.SkuInfo;
+import com.xytg.model.product.SkuPoster;
+import xytg.model.product.*;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class SkuInfoVo extends SkuInfo {
+
+	@ApiModelProperty(value = "海报列表")
+	private List<SkuPoster> skuPosterList;
+
+	@ApiModelProperty(value = "属性值")
+	private List<SkuAttrValue> skuAttrValueList;
+
+	@ApiModelProperty(value = "图片")
+	private List<SkuImage> skuImagesList;
+
+}
+
---
+++ service/service-acl/src/main/java/com/gduf/xytg/ServiceAclApplication.java
@@ -0,0 +1,10 @@
+package com.gduf.xytg;
+
+/**
+ * @author LuoXuanwei
+ * @version 1.0
+ * @description 启动类
+ * @date 2025/10/18 22:22
+ */
+public class ServiceAclApplication {
+}
